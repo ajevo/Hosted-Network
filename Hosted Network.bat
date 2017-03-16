04263f807c8fcfa2b5b44ffb4ea47bcafa1ca926
ajevo <adjustedevolution@gmail.com>
2017-03-16 16:37:08 +0000

    Add files via upload

--- Hosted Network.bat
+++ Hosted Network.bat
@@ -5,3 +5,6 @@ exit
 
 :: netsh wlan stop hostednetwork
 :: netsh wlan show hostednetwork
+:: netsh wlan delete profile name="n00bcake"
+:: netsh wlan set hostednetwork mode=allow ssid=n00bcake key=
+:: netsh wlan show profiles
